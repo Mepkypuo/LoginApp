cbfdd942d2834b0f1fda302fc20f71e29969657d
Maxim Zhilyaev <Zhilyaev.ms74@gmail.com>
2022-11-06 14:26:02 +0300

    Доработка после замечаний учителя

--- LoginApp/LoginViewController.swift
+++ LoginApp/LoginViewController.swift
@@ -11,16 +11,8 @@ class LoginViewController: UIViewController {
     @IBOutlet var userNameTF: UITextField!
     @IBOutlet var passwordTF: UITextField!
     
-    @IBOutlet var loginButton: UIButton!
-    
-    @IBOutlet var forgotUsernameButton: UIButton!
-    @IBOutlet var forgotPasswordButton: UIButton!
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
+    private let userName = "Max"
+    private let password = "123"
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
@@ -28,29 +20,25 @@ class LoginViewController: UIViewController {
     }
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         super .touchesBegan(touches, with: event)
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
     
     @IBAction func forgotUsernameButtonPressed() {
-        showAlert(withTitle: "Your User Name", andMessage: "Max")
+        showAlert(withTitle: "Your User Name", andMessage: userName)
     }
     @IBAction func forgotPasswordButtonPressed() {
-        showAlert(withTitle: "Your Password", andMessage: "123")
+        showAlert(withTitle: "Your Password", andMessage: password)
     }
     @IBAction func loginButtonPressed() {
-        switch (userNameTF.text, passwordTF.text) {
-        case ("Max","123"):
-            performSegue(withIdentifier: "Welcome", sender: nil)
-        default:
-            showAlert(
-                withTitle: "Invalid UserName or Password",
-                andMessage: "Please, enter correct login or password")
-           }
+        if (userNameTF.text, passwordTF.text) == (userName, password) { performSegue(withIdentifier: "Welcome", sender: nil)
+        } else { showAlert(
+            withTitle: "Invalid UserName or Password",
+            andMessage: "Please, enter correct login or password"
+        )
+        }
     }
     
     @IBAction func unwind(for segue: UIStoryboardSegue) {
-        guard let _ = segue.source as? WelcomeViewController else { return }
-        userNameTF.text = ""
         passwordTF.text = ""
     }
 }
@@ -61,7 +49,6 @@ extension LoginViewController {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
         let okAction = UIAlertAction(title: "OK", style: .default) {
             _ in
-            self.userNameTF.text = ""
             self.passwordTF.text = ""
         }
         alert.addAction(okAction)
